ce9ff264be9af2781078402f21f6ba88699a1a26
Fred010 <fjbaafi18@gmail.com>
2025-10-24 04:40:34 +0000

    touches on files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,47 @@
-const app = require('./app');
-const connectMongoDB = require('./config/db');
-const PORT = Process.env.PORT || 5000;
+import dotenv from "dotenv";
+dotenv.config();
 
-// Connecting to MongoDB
-connectMongoDB();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-// Starting the server
+// routes
+import userAuthRoutes from "./routes/userAuth.js";
+import adminAuthRoutes from "./routes/adminAuth.js";
+import companyAuthRoutes from "./routes/companyAuth.js";
+
+const app = express();
+
+// middleware for parsing JSON and CORS
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Auth Service is running");
+});
+
+// use routes
+app.use("/api/user", userAuthRoutes);
+app.use("/api/admin", adminAuthRoutes);
+app.use("/api/company", companyAuthRoutes);
+
+// connect to MongoDB and start server
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error" });
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Trekka backend server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
